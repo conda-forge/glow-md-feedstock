cbd0baa2cfe2f14961f72c8a7a6f03b568b03729
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 13:51:17 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,10 @@
-go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\glow.exe" -ldflags="-s -w -X main.Version=%PKG_VERSION%" || goto :error
+@echo on
+@setlocal EnableDelayedExpansion
+
+go build -o="%LIBRARY_BIN%\glow.exe" -ldflags="-s -w -X main.Version=%PKG_VERSION%" || goto :error
 go-licenses save . --save_path=license-files --ignore github.com/mattn/go-localereader || goto :error
 
-goto :EOF
+goto :eof
 
 :error
 echo Failed with error #%errorlevel%.
